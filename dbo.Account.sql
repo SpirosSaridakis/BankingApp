e66601668a5890c6bece8fcc4c6dfaa7173c894a
Giannis Generalis <81437113+ioanngen@users.noreply.github.com>
2022-12-05 20:16:27 +0200

    SQL for Bank Accounts

---
+++ dbo.Account.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Account] (
+    [Id]       INT            IDENTITY (1, 1) NOT NULL,
+    [desc]     NVARCHAR (MAX) NULL,
+    [balance]  FLOAT (53)     NULL,
+    [currency] NVARCHAR (MAX) NOT NULL,
+    CONSTRAINT [PK_Account] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
