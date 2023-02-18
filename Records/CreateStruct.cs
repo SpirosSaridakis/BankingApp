ea2dc699750868f337b6922bbb3603e0fb705763
SpirosSaridakis <spiros.sar1234@hotmail.com>
2023-02-18 19:20:53 +0200

    Added structs to represent the request sent to the controllers and created some json files for testing purposes

---
+++ Records/CreateStruct.cs
@@ -0,0 +1,9 @@
+namespace Johns_App.Records
+{
+    public struct CreateAccountRequest{
+        string desc;
+        string currency;
+        double balance;
+        int userid;
+    }
+}
---
+++ Records/TransactionRequest.cs
@@ -0,0 +1,8 @@
+using System;
+namespace Johns_App.Records
+{
+    public struct TransactionRequest{
+        Guid AccountId;
+        double ammount;
+    }
+}
